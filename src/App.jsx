db0fc61018a24226f39f8b64476c47edb267f751
SoQu-PJ <maniek999zano@gmail.com>
2023-03-18 15:59:56 +0100

    add html, style, rwd

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,28 @@ import './App.css'
 function App() {
 
   return (
-    <h1>ss</h1>
+    <main className='calculator'>
+      <section id='display' className='display'>111</section>
+      <section className='buttons'>
+        <button id='clear'>AC</button>
+        <button id='divide' className='arithmetic-signs'>/</button>
+        <button id='multiply' className='arithmetic-signs'>X</button>
+        <button id='subtract' className='arithmetic-signs'>-</button>
+        <button id='add' className='arithmetic-signs'>+</button>
+        <button id='nine'>9</button>
+        <button id='eight'>8</button>
+        <button id='seven'>7</button>
+        <button id='six'>6</button>
+        <button id='five'>5</button>
+        <button id='four'>4</button>
+        <button id='three'>3</button>
+        <button id='two'>2</button>
+        <button id='one'>1</button>
+        <button id='zero'>0</button>
+        <button id='decimal'>.</button>
+        <button id='equals'>=</button>
+      </section>
+    </main>
   );
 }
 
