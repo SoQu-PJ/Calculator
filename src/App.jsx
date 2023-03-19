65ab17c1473ed6b9869c8855656c02a78cbbfd46
SoQu-PJ <maniek999zano@gmail.com>
2023-03-19 13:05:38 +0100

    add event to button with class 'display-button'

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [displayValue, setDisplayValue] = useState(["0"]);
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
+
+  const setEquation = e =>{
+    setDisplayValue();
+  } 
+
+  useEffect(()=>{
+    const displayButton = document.querySelectorAll(".display-button");
+    
+
+    displayButton.forEach(el => {
+      el.addEventListener("click", setEquation);
+      return () => el.removeEventListener("click", setEquation);
+    });
+
+  });
 
   return (
     <main className='calculator'>
-      <section id='display' className='display'>111</section>
+      <section id='display' className='display'>{displayValue}</section>
       <section className='buttons'>
-        <button id='clear'>AC</button>
-        <button id='divide' className='arithmetic-signs'>/</button>
-        <button id='multiply' className='arithmetic-signs'>X</button>
-        <button id='subtract' className='arithmetic-signs'>-</button>
-        <button id='add' className='arithmetic-signs'>+</button>
-        <button id='nine'>9</button>
-        <button id='eight'>8</button>
-        <button id='seven'>7</button>
-        <button id='six'>6</button>
-        <button id='five'>5</button>
-        <button id='four'>4</button>
-        <button id='three'>3</button>
-        <button id='two'>2</button>
-        <button id='one'>1</button>
-        <button id='zero'>0</button>
-        <button id='decimal'>.</button>
+        <button id='clear' onClick={() => setDisplayValue(["0"])}>AC</button>
+
+        <button id='divide' className='display-button arithmetic-signs'>/</button>
+        <button id='multiply' className='display-button arithmetic-signs'>X</button>
+        <button id='subtract' className='display-button arithmetic-signs'>-</button>
+        <button id='add' className='display-button arithmetic-signs'>+</button>
+        
+        <button id='nine' className='display-button number'>9</button>
+        <button id='eight' className='display-button number'>8</button>
+        <button id='seven' className='display-button number'>7</button>
+        <button id='six' className='display-button number'>6</button>
+        <button id='five' className='display-button number'>5</button>
+        <button id='four' className='display-button number'>4</button>
+        <button id='three' className='display-button number'>3</button>
+        <button id='two' className='display-button number'>2</button>
+        <button id='one' className='display-button number'>1</button>
+        <button id='zero' className='display-button number'>0</button>
+        <button id='decimal' className='display-button number'>.</button>
         <button id='equals'>=</button>
       </section>
     </main>
