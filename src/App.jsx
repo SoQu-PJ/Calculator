f37080b3272c2b9fe3488d2615ab2e1b627c5e24
SoQu-PJ <maniek999zano@gmail.com>
2023-03-25 14:52:55 +0100

    repair bug with ArithmeticHandler and EquationHandler

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [displayValue, setDisplayValue] = useState(['0']);
-  const [arithmeticSigns, setArithmeticSigns] = useState({signs: '', use: false})
+  const [arithmeticSigns, setArithmeticSigns] = useState('')
   const [firstNumber, setFirstNumber] = useState(['0']);
   const [secondNumber, setSecondNumber] = useState(['0']);
 
@@ -11,52 +11,54 @@ function App() {
     setDisplayValue(['0']);
     setFirstNumber(['0']);
     setSecondNumber(['0']);
-    setArithmeticSigns({signs: '', use: false});
+    setArithmeticSigns('');
   }
 
-  const setEquation = e =>{
-    !arithmeticSigns.use ? setFirstNumber(prev => [...prev, e.target.textContent]) : setSecondNumber(prev => [...prev, e.target.textContent]);
+  const setEquationHandler = e => {
     setDisplayValue(prev => [...prev, e.target.textContent]);
+    arithmeticSigns === '' ? setFirstNumber(prev => [...prev, e.target.textContent]) : setSecondNumber(prev => [...prev, e.target.textContent]);
+    
   } 
-  
-  useEffect(()=>{
-    const displayButton = document.querySelectorAll(".display-button");
-    
-    
-    displayButton.forEach(el => {
-      el.addEventListener("click", setEquation);
-      
-      return ()=> el.removeEventListener("click", setEquation)
-    });
-  }, []);
 
+  const setArithmeticSignsHandler = e => {
+    setDisplayValue(prev => [...prev, arithmeticSigns]); 
+    setArithmeticSigns(e.target.dataset.sign);
+  }
+
+  const equalsHandler = () => {
+    
+  }
+
+  useEffect(()=>{
+
+  })
 
   return (
     <main className='calculator'>
       <section id='display' className='display'>
         <p className='smallNumbers'>{displayValue.join("").replace(/^0+/,"")}</p>
-        <p className='selectNumber'>{!arithmeticSigns.use ? parseFloat(firstNumber.join("")) : parseFloat(secondNumber.join(""))}</p>
+        <p className='selectNumber'>{arithmeticSigns === '' ? parseFloat(firstNumber.join("")) : parseFloat(secondNumber.join(""))}</p>
       </section>
       <section className='buttons'>
         <button id='clear' onClick={clearHandler}>AC</button>
 
-        <button id='divide' className='display-button arithmetic-signs' onClick={() => setArithmeticSigns({signs: "/", use: true})}>/</button>
-        <button id='multiply' className='display-button arithmetic-signs' onClick={() => setArithmeticSigns({signs: "*", use: true})}>X</button>
-        <button id='subtract' className='display-button arithmetic-signs' onClick={() => setArithmeticSigns({signs: "-", use: true})}>-</button>
-        <button id='add' className='display-button arithmetic-signs' onClick={() => setArithmeticSigns({signs: "+", use: true})}>+</button>
+        <button id='divide' className='display-button arithmetic-signs' data-sign="/" onClick={setArithmeticSignsHandler}>/</button>
+        <button id='multiply' className='display-button arithmetic-signs' data-sign="*" onClick={setArithmeticSignsHandler}>X</button>
+        <button id='subtract' className='display-button arithmetic-signs' data-sign="-" onClick={setArithmeticSignsHandler}>-</button>
+        <button id='add' className='display-button arithmetic-signs' data-sign="+" onClick={setArithmeticSignsHandler}>+</button>
         
-        <button id='nine' className='display-button number'>9</button>
-        <button id='eight' className='display-button number'>8</button>
-        <button id='seven' className='display-button number'>7</button>
-        <button id='six' className='display-button number'>6</button>
-        <button id='five' className='display-button number'>5</button>
-        <button id='four' className='display-button number'>4</button>
-        <button id='three' className='display-button number'>3</button>
-        <button id='two' className='display-button number'>2</button>
-        <button id='one' className='display-button number'>1</button>
-        <button id='zero' className='display-button number'>0</button>
-        <button id='decimal' className='display-button number'>.</button>
-        <button id='equals'>=</button>
+        <button id='nine' className='display-button number' onClick={setEquationHandler}>9</button>
+        <button id='eight' className='display-button number' onClick={setEquationHandler}>8</button>
+        <button id='seven' className='display-button number' onClick={setEquationHandler}>7</button>
+        <button id='six' className='display-button number' onClick={setEquationHandler}>6</button>
+        <button id='five' className='display-button number' onClick={setEquationHandler}>5</button>
+        <button id='four' className='display-button number' onClick={setEquationHandler}>4</button>
+        <button id='three' className='display-button number' onClick={setEquationHandler}>3</button>
+        <button id='two' className='display-button number' onClick={setEquationHandler}>2</button>
+        <button id='one' className='display-button number' onClick={setEquationHandler}>1</button>
+        <button id='zero' className='display-button number' onClick={setEquationHandler}>0</button>
+        <button id='decimal' className='display-button number' onClick={setEquationHandler}>.</button>
+        <button id='equals' onClick={equalsHandler}>=</button>
       </section>
     </main>
   );
